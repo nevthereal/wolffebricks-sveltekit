2729ec76838424647c9158cf95432271254c4ae1
Neville Brem <bremneville@gmail.com>
2024-01-31 14:08:28 +0100

    feat: added anchor to cartproducts

--- src/lib/components/cartProduct.svelte
+++ src/lib/components/cartProduct.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { focusTrap } from '@skeletonlabs/skeleton';
 	import { removeFromCart } from '../cart';
 	import { getProductData } from '../products';
 
@@ -13,13 +12,13 @@
 </script>
 
 <div class="flex justify-between card p-4 my-2">
-	<div class="flex gap-8 items-center">
+	<a href={`/products/${item.slug}`} class="flex gap-8 items-center">
 		<img src={item.img_main} alt={item.title} class="h-24 rounded-token hidden md:block" />
 		<div class="my-auto text-left">
 			<h3 class="h3 my-auto font-bold">{item.title}</h3>
 			<h4 class="h4">CHF {item.price}</h4>
 		</div>
-	</div>
+	</a>
 	<button class="btn h4 my-auto" on:click={() => removeFromCart(item.id)}>
 		<i class="fa-solid fa-trash"></i>
 	</button>
