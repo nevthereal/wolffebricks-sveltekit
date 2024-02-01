4602113fe35799c9913af4eeccf16bc74d9a7f5b
Neville Brem <bremneville@gmail.com>
2024-02-01 09:10:27 +0100

    feat: added terms and privacy policy pages

---
+++ src/routes/terms-of-service/+page.svelte
@@ -0,0 +1,138 @@
+<div>
+	<h1 class="text-5xl md:text-6xl font-bold mb-8">Terms of Service</h1>
+	<h2 class="text-base md:text-lg italic font-mono">Effective Date: 28. July</h2>
+	<div class="mx-16 md:max-w-[40%] md:mx-auto">
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">1. Definitions:</h2>
+			<p></p>
+			<p>1.1. "We," "us," or "Wolffe Bricks" refers to the operator of this online store.</p>
+			<p>1.2. "You" or "user" refers to anyone who accesses and uses our online store.</p>
+			<p>
+				1.3. "Product(s)" refers to any goods, merchandise, or services offered for sale on our
+				website.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">2. Use of the Online Store:</h2>
+			<p>
+				2.1. Eligibility: You must be the legal age of majority in your jurisdiction to use our
+				online store. By accessing and using our website, you confirm that you meet these
+				eligibility requirements.
+			</p>
+			<p>
+				2.2. Account Creation: To make a purchase on our online store, you are required to create an
+				account. You are responsible for providing accurate and up-to-date information during the
+				registration process. Keep your account credentials secure and do not share them with
+				others.
+			</p>
+			<p>
+				2.3. Compliance with Laws: You agree to use our online store and make purchases in
+				compliance with all applicable laws, regulations, and these Terms and Conditions.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">3. Product Listings and Pricing:</h2>
+			<p>
+				3.1. Product Descriptions: We strive to provide accurate product descriptions, images, and
+				details on our website. However, we do not guarantee that the information is error-free,
+				complete, or up-to-date. We reserve the right to modify product information at any time
+				without prior notice.
+			</p>
+			<p>
+				3.2. Product Availability: Product availability is subject to change without notice. If a
+				product you ordered is unavailable, we will notify you and offer a suitable alternative or a
+				refund for that product.
+			</p>
+			<p>
+				3.3. Pricing: Product prices are listed in CHF (Swiss Franc) and are subject to change
+				without prior notice. The final purchase price will be the price displayed on the website at
+				the time of checkout.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">4. Ordering and Payment:</h2>
+			<p>
+				4.1. Order Acceptance: Placing an order does not guarantee its acceptance. We reserve the
+				right to accept or decline any order in our sole discretion. If your order is confirmed, you
+				will get an email.
+			</p>
+			<p>
+				4.2. Payment: All payments must be made using the payment methods provided on our website.
+				You agree to provide accurate billing information and authorize us to charge your selected
+				payment method for the total order amount. See{' '}
+				<a href="/info" class="font-bold"> Info </a>
+			</p>
+			<p>
+				4.3. Order Cancellation: Once an order is submitted, it cannot be canceled or modified.
+				Please review your order carefully before completing the purchase.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">5. Shipping and Delivery:</h2>
+			<p>
+				5.1. Shipping: We will make reasonable efforts to ship products to the address provided
+				during checkout within the estimated delivery timeframe. However, we are not responsible for
+				any delays or issues caused by the shipping carrier or events beyond our control.
+			</p>
+			<p>
+				5.2. Delivery: Title and risk of loss for products pass to you upon delivery. Please inspect
+				the delivered products and notify us of any damages or discrepancies within 30 days of
+				receipt.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">6. Returns and Refunds:</h2>
+			<p>6.1. Returns: You are not eligible to return your products once shipped.</p>
+			<p>
+				6.2. Refunds: If your order hasn't been sent yet, you can request a refund at{' '}
+				<a href="mailto:support@wolffebricks.store"> support@wolffebricks.store </a>
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">7. Intellectual Property:</h2>
+			<p>
+				7.1. Ownership: All content, trademarks, logos, and intellectual property displayed on our
+				website are the property of Wolffe Bricks or their respective owners. You may not use,
+				reproduce, or distribute any content without prior written permission.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">8. Limitation of Liability:</h2>
+			<p>
+				8.1. To the fullest extent permitted by law, Wolffe Bricks shall not be liable for any
+				direct, indirect, incidental, consequential, or special damages arising from your use of our
+				online store, products, or any third-party content or services linked to our website.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">9. Indemnification:</h2>
+			<p>
+				9.1. You agree to indemnify and hold Wolffe Bricks harmless from any claims, damages,
+				losses, or expenses arising out of your breach of these Terms and Conditions or your use of
+				our online store.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">10. Modifications to Terms:</h2>
+			<p>
+				10.1. We reserve the right to update or modify these Terms and Conditions at any time. Any
+				changes will be effective immediately upon posting on our website. Your continued use of our
+				online store after such changes constitutes your acceptance of the updated terms.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2 class="text-lg md:text-xl font-semibold">11. Governing Law and Jurisdiction:</h2>
+			<p>
+				11.1. These Terms and Conditions shall be governed by and construed in accordance with the
+				laws of Switzerland. Any disputes arising out of these terms shall be subject to the
+				exclusive jurisdiction of the courts located in Zurich.
+			</p>
+		</div>
+	</div>
+	<p>
+		If you have any questions or concerns regarding these Terms and Conditions, please contact us at{' '}
+		<a href="mailto:support@wolffebricks.store"> support@wolffebricks.store </a>
+		.
+	</p>
+	<p>This Document was last updated on 28. July.</p>
+</div>
